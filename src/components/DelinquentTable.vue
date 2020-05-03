be2960273b5f59c081244430856cf0b1c62e929e
juyeol <ljykimc159@gmail.com>
2020-05-03 18:48:50 +0900

    테이블 컬럼 추가

--- src/components/DelinquentTable.vue
+++ src/components/DelinquentTable.vue
@@ -23,26 +23,26 @@
 <script>
 const columns = [
   { title: '구분', dataIndex: 'index', key: 'index'},
-  { title: '월세 미납분', dataIndex: 'key1', key: 'key1'},
+  { title: '월세 미납분', dataIndex: 'nonPayMonthly', key: 'nonPayMonthly'},
   { title: '승인일', dataIndex: 'key2', key: 'key2'},
   { title: '대리점', dataIndex: 'company.companyName', key: 'company.companyName'},
   { title: '대리점 계약번호', dataIndex: 'key3', key: 'key3'},
   { title: '승인번호', dataIndex: 'key4', key: 'key4'},
   { title: '계약자(영문)', dataIndex: 'key5', key: 'key5'},
-  { title: '계약자', dataIndex: 'contractorName', key: 'contractorName'},
-  { title: '멘션명', dataIndex: 'propertyName', key: 'propertyName'},
-  { title: '방번호', dataIndex: 'roomNumber', key: 'roomNumber'},
-  { title: '국내 핸드폰 번호', dataIndex: 'contractorTel', key: 'contractorTel'},
-  { title: '청구액', dataIndex: 'key6', key: 'key6' },
-  { title: '월세', dataIndex: 'rent', key: 'rent'},
-  { title: '수수료', dataIndex: 'key7', key: 'key7' },
-  { title: '체납발생', dataIndex: 'key8', key: 'key8' },
-  { title: '담당자', dataIndex: 'comfirmPerson', key: 'comfirmPerson'},
+  { title: '계약자', dataIndex: 'user.contractorName', key: 'user.contractorName'},
+  { title: '멘션명', dataIndex: 'user.propertyName', key: 'user.propertyName'},
+  { title: '방번호', dataIndex: 'user.roomNumber', key: 'user.roomNumber'},
+  { title: '연락처', dataIndex: 'user.contractorTel', key: 'user.contractorTel'},
+  { title: '청구액', dataIndex: 'charges', key: 'charges' },
+  { title: '월세', dataIndex: 'user.rent', key: 'user.rent'},
+  { title: '수수료', dataIndex: 'fees', key: 'fees' },
+  { title: '체납발생', dataIndex: 'arrears', key: 'arrears' },
+  { title: '담당자', dataIndex: 'user.comfirmPerson', key: 'user.comfirmPerson'},
   { title: '은행명', dataIndex: 'company.bankName', key: 'company.bankName'},
   { title: '지점명', dataIndex: 'company.branchOfficeName', key: 'company.branchOfficeName'},
   { title: '계좌번호', dataIndex: 'company.bankAccountNumber', key: 'company.bankAccountNumber'},
   { title: '비고', dataIndex: 'company.notes', key: 'company.notes'},
-  { title: '긴급연락처', dataIndex: 'emergencyTel1', key: 'emergencyTel1'}
+  { title: '긴급연락처', dataIndex: 'user.emergencyTel1', key: 'user.emergencyTel1'}
 ];
 const object = {
   //월세 미납분
@@ -57,37 +57,14 @@ export default {
   data() {
     return {
       columns,
-      windowSize:{x:'max-content'},
-      delinquentList:[{
-        'index': '1',
-        'key1': '2',
-        'key2': '3',
-        'company.companyName':'4',
-        'key3': '5',
-        'key4': '6',
-        'key5': '7',
-        'contractorName': '8',
-        'propertyName': '9',
-        'roomNumber': '10',
-        'contractorTel': '11',
-        'key6': '12',
-        'rent': '13',
-        'key7': '14',
-        'key8': '15',
-        'comfirmPerson': '16',
-        'company.bankName': '17',
-        'company.branchOfficeName': '18',
-        'company.bankAccountNumber': '19',
-        'company.notes': '20',
-        'emergencyTel1': '21'
-      }]
+      windowSize:{x:'max-content'}
     };
   },
-  // computed: {
-  //   ...mapGetters({
-  //     delinquentList:"getAllDelinquentList",
-  //   })
-  // },
+  computed: {
+    ...mapGetters({
+      delinquentList:"getAllDelinquentList",
+    })
+  },
   mounted(){
     this.db = firebase.firestore();
     const y = window.innerHeight-300;
