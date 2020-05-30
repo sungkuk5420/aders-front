79e08a855cf4d751da2246e55876677f02344260
sungkuk5420 <sungkuk5420@gmail.com>
2020-05-31 03:30:21 +0900

    빌드파일 업로드

--- src/pages/addDelinquentPage.vue
+++ src/pages/addDelinquentPage.vue
@@ -1982,6 +1982,7 @@ export default {
           blackList: true
         })
         .then(function(docRef) {
+          thsiObj.$sotre.dispatch(T.GET_BLACK_LIST);
           thisObj.searchedUser.blackList = true;
           thisObj.alertMsg({ type: "success", msg: "블랙 리스트 추가 완료" });
           thisObj.loading = false;
